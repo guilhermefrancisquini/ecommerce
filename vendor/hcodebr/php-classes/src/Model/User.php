58b4b7246fd214a88d535ad8ae1aa22513203a37
Guilherme Francisquini <guilherme.francisquini@hotmail.com>
2017-11-02 15:28:45 -0200

    erro ao enviar email gmail

--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -10,11 +10,13 @@ namespace Hcode\Model;
 
 
 use \Hcode\DB\Sql;
+use \Hcode\Mailer;
 use \Hcode\Model;
 
 class User extends Model
 {
     const SESSION = "User";
+    const SECRET = "HcodePhp7_Secret";
 
     public static function login($login, $password)
     {
@@ -118,5 +120,43 @@ class User extends Model
         $sql->select("CALL sp_users_delete(:iduser)", array(
             ":iduser"=>$this->getiduser(),
         ));
+    }
+
+    public static function getForgot($email)
+    {
+        $sql = new Sql();
+        $result = $sql->select("SELECT * FROM tb_persons a INNER JOIN tb_users b USING(idperson) WHERE a.desemail = :email;", array(
+            ":email"=>$email,
+        ));
+
+        if (count($result) === 0 )
+        {
+            throw new \Exception("Não foi possivel recuperar a senha", 1);
+        }
+        else
+        {
+            $data = $result[0];
+            $result2 = $sql->select("CALL sp_userspasswordsrecoveries_create(:iduser, :desip)", array(
+                ":iduser"=>$data['iduser'],
+                ":desip"=>$_SERVER['REMOTE_ADDR'],
+            ));
+        }
+
+        if (count($result2) === 0)
+        {
+            throw new \Exception("Não possível recuperar a senha");
+        }
+        else
+        {
+            $dataRecovery = $result2[0];
+            $code = base64_encode(mcrypt_encrypt(MCRYPT_RIJNDAEL_128, User::SECRET, $dataRecovery["idrecovery"], MCRYPT_MODE_ECB));
+            $link = "http://franciscommerce.dev/admin/forgot/reset?code=$code";
+
+            $mailer = new Mailer($data["desemail"], $data["desperson"], "Redefinir Senha da Hcode Store", "forgot", array(
+                "name" => $data["desperson"],
+                "link" => $link,
+            ));
+            $mailer->send();
+        }
     }
 }
